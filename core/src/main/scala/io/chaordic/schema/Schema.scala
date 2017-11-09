0faa60507c600f86707c21b62c60be145ea4f823
Wille Faler <wille.faler@gmail.com>
2017-11-09 22:40:30 +0100

    add primitive

--- core/src/main/scala/io/chaordic/schema/Schema.scala
+++ core/src/main/scala/io/chaordic/schema/Schema.scala
@@ -3,20 +3,23 @@ package io.chaordic.schema
 
 sealed trait SchemaVal
 
+sealed trait Primitive extends SchemaVal
+sealed trait Complex extends SchemaVal
+
 object SchemaVal {
-  case class Str(value: String) extends SchemaVal
-  case class IntNum(value: Int) extends SchemaVal
-  case class LongNum(value: Long) extends SchemaVal
-  case class DoubleNum(value: Double) extends SchemaVal
-  case class Bool(value: Boolean) extends SchemaVal
-  case class Obj(value: List[(String, SchemaVal)]) extends SchemaVal{
+  case class Str(value: String) extends Primitive
+  case class IntNum(value: Int) extends Primitive
+  case class LongNum(value: Long) extends Primitive
+  case class DoubleNum(value: Double) extends Primitive
+  case class Bool(value: Boolean) extends Primitive
+  case class Obj(value: List[(String, SchemaVal)]) extends Complex{
     def get(key: String): Option[SchemaVal] = {
       value.find(_._1 == key).map(_._2)
     }
   }
 
-  case class SList(value: List[SchemaVal]) extends SchemaVal
-  case object Null extends SchemaVal
+  case class SList(value: List[SchemaVal]) extends Complex
+  case object Null extends Primitive
 
   val Empty = Obj(Nil)
 }
